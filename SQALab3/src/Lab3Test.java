d6e07a052783f7f8e2570a5601200cfd8a18d46e
LadyMopar <richarel@rose-hulman.edu>
2015-03-24 14:50:09 -0400

    upload

---
+++ SQALab3/src/Lab3Test.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author richarel.
+ *         Created Mar 24, 2015.
+ */
+public class Lab3Test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
