3fa141ebba73c438b5ea64e739d406f8958b241c
LadyMopar <richarel@rose-hulman.edu>
2015-03-26 14:11:55 -0400

    Added method to be tested. Passed the first test.

--- SQALab3/src/Task5.java
+++ SQALab3/src/Task5.java
@@ -3,11 +3,21 @@
  */
 
 /**
- * TODO Put here a description of what this class does.
- *
- * @author richarel.
- *         Created Mar 26, 2015.
+ * This is where I wrote the method to be tested.
+ * 
+ * @author richarel. Created Mar 26, 2015.
  */
 public class Task5 {
+
+	/**
+	 * Generates a list of prime numbers that are strictly smaller that the
+	 * input.
+	 * 
+	 * @param input
+	 * @return
+	 */
+	public static Object generatePrime(Integer input) {
+		return null;
+	}
 
 }
