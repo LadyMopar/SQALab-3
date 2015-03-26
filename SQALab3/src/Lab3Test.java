8689ee278f8165344d41b2220ca573d630f94073
LadyMopar <richarel@rose-hulman.edu>
2015-03-25 23:45:39 -0400

    decided to add in some throw exceptions

--- SQALab3/src/Lab3Test.java
+++ SQALab3/src/Lab3Test.java
@@ -23,42 +23,42 @@ public class Lab3Test {
 	}
 
 	@Test
-	public void testReturnEmptyFor1() {
+	public void testReturnEmptyFor1() throws Exception {
 		assertEquals(list(), Lab3.primeFact(1));
 	}
 
 	@Test
-	public void testReturn2For2() {
+	public void testReturn2For2() throws Exception {
 		assertEquals(list(2), Lab3.primeFact(2));
 	}
 	
 	@Test
-	public void testReturn3For3() {
+	public void testReturn3For3() throws Exception {
 		assertEquals(list(3), Lab3.primeFact(3));
 	}
 	
 	@Test
-	public void test4(){
+	public void test4() throws Exception{
 		assertEquals(list(2,2), Lab3.primeFact(4));
 	}
 	
 	@Test
-	public void test6(){
+	public void test6() throws Exception{
 		assertEquals(list(2,3), Lab3.primeFact(6));
 	}
 	
 	@Test
-	public void test8(){
+	public void test8() throws Exception{
 		assertEquals(list(2,2,2), Lab3.primeFact(8));
 	}
 	
 	@Test
-	public void test9(){
+	public void test9() throws Exception{
 		assertEquals(list(3,3), Lab3.primeFact(9));
 	}
 	
 	@Test
-	public void test325115(){
+	public void test325115() throws Exception{
 		assertEquals(list(5, 7, 7, 1327), Lab3.primeFact(325115));
 	}
 
