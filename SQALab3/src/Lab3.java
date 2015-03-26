20dc6224f6789a25f98aeada6e012dcb5d5ad251
LadyMopar <richarel@rose-hulman.edu>
2015-03-25 23:42:49 -0400

    refactored for the 9 test case

--- SQALab3/src/Lab3.java
+++ SQALab3/src/Lab3.java
@@ -7,27 +7,25 @@ import java.util.List;
 
 /**
  * Writing method to be tested.
- *
- * @author richarel.
- *         Created Mar 24, 2015.
+ * 
+ * @author richarel. Created Mar 24, 2015.
  */
 public class Lab3 {
-	
 
 	/**
 	 * Returns a list of prime factors of a given integer.
-	 *
+	 * 
 	 * @param i
 	 * @return a string of factors
 	 */
 	public static List<Integer> primeFact(int i) {
 		ArrayList<Integer> list = new ArrayList<Integer>();
-		while(i % 2 == 0){
-			list.add(2);
-			i/=2;
-		}
-		if(i>1){
-			list.add(i);
+		int factor = 2;
+		while (i > 1) {
+			for (; i % factor == 0; i /= factor) {
+				list.add(factor);
+			}
+			factor++;
 		}
 		return list;
 	}
