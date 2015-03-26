9873c7ed1290b75f65a422acd97487d2d11c7afb
LadyMopar <richarel@rose-hulman.edu>
2015-03-26 13:55:00 -0400

    Added the two new classes for task 5

---
+++ SQALab3/src/Task5.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author richarel.
+ *         Created Mar 26, 2015.
+ */
+public class Task5 {
+
+}
---
+++ SQALab3/src/Task5Test.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author richarel.
+ *         Created Mar 26, 2015.
+ */
+public class Task5Test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
